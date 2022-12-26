a44e8cc33a47aea69c5e7e3b87e07becbdcb1184
jagadish <jagadish.nadar41@gmail.com>
2022-12-26 12:45:19 +0530

    vercel settinigs done

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,10 +123,10 @@ STATIC_URL = 'static/'
 if DEBUG:
    STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'static'),
-   os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+#    os.path.join(BASE_DIR, 'staticfiles_build', 'static')
    ]
 else:
-   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+   STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
