6e33ad0817dd30336e19108ffffc6ea7069d6c56
jagadish <jagadish.nadar41@gmail.com>
2022-12-26 14:22:43 +0530

    vercel settinigs done

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
-
 from django.urls import path
 from . import views
 urlpatterns = [
