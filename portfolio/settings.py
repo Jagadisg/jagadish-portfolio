4fb66134a8f3c2de5ab10fda869de3f4d9cfbfcd
jagadish <jagadish.nadar41@gmail.com>
2022-12-26 12:39:26 +0530

    vercel settinigs done

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-n6c2o@qu+huq67!pdh9jg6xq26dh6k9v78y5x)t4-xyn22e&s!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','jagadishportfolio.herokuapp.com']
+# ALLOWED_HOSTS = ['127.0.0.1','jagadishportfolio.herokuapp.com']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -122,6 +123,7 @@ STATIC_URL = 'static/'
 if DEBUG:
    STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'static'),
+   os.path.join(BASE_DIR, 'staticfiles_build', 'static')
    ]
 else:
    STATIC_ROOT = os.path.join(BASE_DIR,'static')
--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -14,4 +14,5 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
+app = application
+# application = WhiteNoise(application)
